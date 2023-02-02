6946d843ea135880d7af28fcd4ab6dd2d41831e8
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-02-02 16:22:48 -0500

    Se realiza ajuste al script de la tarea 1 y tarea 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,15 +4,6 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 //Create a gallery with the ability to click on its items and view full size images in a modal window
 
-//Do this task in the 01-gallery.html and 01-gallery.js files. Break it down into several subtasks:
-//1. Creating and rendering markup from the galleryItems data array and provided gallery item template.
-//2. Implementing delegation to div.gallery and getting the url of a large image.
-//3. Adding the script and styles of the modal window library basicLightbox.
-//   Use the jsdelivrCDN service and add links to minified (.min) library files to your project.
-//4. Opening a modal window by clicking on a gallery item. To do this, check out the documentation and examples.
-//5. Replacing the value of the src attribute of the <img> element in a modal window before opening. Use the ready-made
-//   modal window markup with the image from the examples of the basicLightbox library.
-
 //gallery container selector
 const containerGallery = document.querySelector(".gallery");
 
@@ -47,6 +38,8 @@ containerGallery.addEventListener("click", selectOriginalImage);
 //another page by default. Disable this behavior by default.
 //Add modal window closing upon pressing the Escape key. Make keyboard listening available only while the modal
 //window is open. In the basicLightbox library, there is a method to close the modal window programmatically.
+//5. Replacing the value of the src attribute of the <img> element in a modal window before opening. Use the ready-made
+//   modal window markup with the image from the examples of the basicLightbox library.
 //image selector 
 function selectOriginalImage(event) {
   event.preventDefault();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,21 +3,6 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-//Create the same gallery as in the first task, but using the SimpleLightbox library, 
-//which will handle image clicks, modal opening and closing and image scrolling with the keyboard.
-
-
-//Do this task in the 02-lightbox.html and 02-lightbox.js files. Break it down into several subtasks:
-
-//1. Creating and rendering markup from the galleryItems data array and provided gallery element template. 
-//   Use the ready-made code from the first task.
-//2. Adding the script and library styles using the cdnjs CDN service. You need to add links to two files: 
-//   simple-lightbox.min.js and simple-lightbox.min.css.
-//3. Library initialization after gallery items are created and added to div.gallery. To do this, read the 
-//   SimpleLightbox documentation - first of all, the Usage and Markup sections.
-//4. Look in the documentation for the Options section and add image caption display from the alt attribute. 
-//   Let the caption be at the bottom and appear 250 milliseconds after image opening.
-
 //list container selector
 const listGallery = document.querySelector(".gallery");
 
@@ -35,17 +20,14 @@ const galleryMarkup = galleryItems
 //   Use the ready-made code from the first task.
 listGallery.insertAdjacentHTML("afterbegin", galleryMarkup);
 
+//3. Library initialization after gallery items are created and added to div.gallery. To do this, read the 
+//   SimpleLightbox documentation - first of all, the Usage and Markup sections.
+//4. Look in the documentation for the Options section and add image caption display from the alt attribute. 
+//   Let the caption be at the bottom and appear 250 milliseconds after image opening.
+listGallery.addEventListener("click", (event) => event.preventDefault());
 
-//library selector listener
-listGallery.addEventListener("click", selectlibrary);
-
-function selectlibrary(event) {
-    event.preventDefault();
-    let lightbox = new SimpleLightbox('.gallery a', { 
-        captionsData: 'alt',
-        captionDelay: 250,
-     });
-    
-    
-  }
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
 
